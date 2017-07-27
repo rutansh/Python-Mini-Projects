056014ba8ee41bb954ba27d38df37bbfe56a8cc0
rutansh <rutanshtrivedi@rocketmail.com>
2017-07-27 19:35:55 +0530

    browser will open and play song after 10 second

---
+++ break_time.py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+total_break=1
+break_count=0
+print("this program started on "+time.ctime())
+while(break_count<total_break):
+        print("breaaakkk time......!!!!!")
+        time.sleep(10)
+        
+        webbrowser.open("https://www.youtube.com/watch?v=whwe0KD_rGw")
+        break_count=break_count+1        
